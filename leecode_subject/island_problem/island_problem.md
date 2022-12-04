dafb86b79874f3d5b991a2d552b878a95b7911b9
mainWj <891277822@qq.com>
2022-12-04 22:23:24 +0800

    [add]新增岛屿问题(岛屿数量，岛屿周长)

---
+++ leecode_subject/island_problem/island_problem.md
@@ -0,0 +1,5 @@
+![image-20221204213818959](C:\Users\伍健\AppData\Roaming\Typora\typora-user-images\image-20221204213818959.png)
+
+
+
+![image-20221204221932129](C:\Users\伍健\AppData\Roaming\Typora\typora-user-images\image-20221204221932129.png)
---
+++ leecode_subject/island_problem/island_project.go
@@ -0,0 +1,66 @@
+package island_problem
+// 此专题用来写leecode上的岛屿问题
+
+// 1. 简单，岛屿数量，使用dfs遍历
+func numIslands(grid [][]byte) int {
+
+	line := len(grid)
+	col := len(grid[0])
+	if line == 0 && col == 0 {
+		return 0
+	}
+	var dfs func(int, int, int, int, [][]byte)
+	dfs = func(x int, y int, line int, col int, grid [][]byte){
+		if x<0 || x >= line || y <0 || y >= col || grid[x][y] != '1'{
+			return
+		}
+		grid[x][y] = '2'
+		dfs(x + 1, y, line, col, grid)
+		dfs(x - 1, y, line, col, grid)
+		dfs(x, y - 1, line, col, grid)
+		dfs(x, y + 1, line, col, grid)
+	}
+	result := 0
+	for i := 0; i < line; i ++ {
+		for j := 0; j < col; j ++{
+			if grid[i][j] == '1'{
+				result ++
+				dfs(i, j, line, col, grid)
+			}
+		}
+	}
+	return result
+}
+
+// 简单  岛屿的周长
+func islandPerimeter(grid [][]int) int {
+	line, col := len(grid), len(grid[0])
+	if line == 0  && col == 0{
+		return 0
+	}
+	var perimeter func(int, int, [][]int)
+	result := 0
+	perimeter = func(x,y int, grid [][]int){
+		if x < 0 || x >= line || y < 0 || y >= col || grid[x][y] == 0{
+			result ++
+			return
+		}
+		if grid[x][y] == 2{
+			return
+		}
+		grid[x][y] = 2
+		perimeter(x + 1, y, grid)
+		perimeter(x - 1, y, grid)
+		perimeter(x, y - 1, grid)
+		perimeter(x, y + 1, grid)
+	}
+	for i:=0;i<line;i++ {
+		for j:=0;j<col;j++{
+			if grid[i][j] == 1{
+				perimeter(i,j,grid)
+				return result
+			}
+		}
+	}
+	return result
+}
